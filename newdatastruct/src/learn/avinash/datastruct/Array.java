fcab506260427ddbee0bdd8d2da4213e4ee80428
Avinash Tiwari <qwe123kids@gmail.com>
2018-08-29 22:59:06 -0600

    Array

---
+++ newdatastruct/src/learn/avinash/datastruct/Array.java
@@ -0,0 +1,24 @@
+package learn.avinash.datastruct;
+
+public class Array {
+
+    public static void main(String[] args) {
+	     int[] intArray = new int[7];
+        intArray[0] = 20;
+
+        intArray[1] = 35;
+
+        intArray[2] = -5;
+
+        intArray[3] = 7;
+
+        intArray[5] = 1;
+
+        intArray[5] = -21;
+
+        for(int i=-0 ; i < intArray.length;i++){
+            System.out.println(i);
+        }
+
+    }
+}
