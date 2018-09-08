4a1a5b0f7884c02ced30dfafcd1532edf0e23af8
Avinash Tiwari <qwe123kids@gmail.com>
2018-09-07 19:32:33 -0600

    no message

---
+++ newdatastruct/src/learn/avinash/datastruct/RecurssionArray.java
@@ -0,0 +1,28 @@
+package learn.avinash.datastruct;
+
+public class RecurssionArray {
+
+
+    public static int iteratieFactorial(int num){
+        if(num ==0){
+            return 1;
+        }
+
+        int factorial =1;
+
+        for(int i  =1 ; i <= num; i++){
+            factorial *=i;
+        }
+
+        return factorial;
+    }
+
+    //n! = n * (n -1)!
+
+    public static int recursiveFactorial(int num){
+        if(num ==0){
+            return 1;
+        }
+return num * recursiveFactorial(num-1);
+    }
+}
