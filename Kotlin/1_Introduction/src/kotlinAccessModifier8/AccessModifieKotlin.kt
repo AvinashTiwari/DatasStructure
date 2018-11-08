57c73345b8c847c514a6f8fe29111c5f7399c96c
Avinash Tiwari <qwe123kids@gmail.com>
2018-11-08 07:59:05 -0700

    no message

--- Kotlin/1_Introduction/src/kotlinAccessModifier8/AccessModifieKotlin.kt
+++ Kotlin/1_Introduction/src/kotlinAccessModifier8/AccessModifieKotlin.kt
@@ -13,6 +13,22 @@ val emp =  Empyloyee("Avinash")
 
     val emp3 =  EmpyloyeeShortway2("Avinash3")
     println(emp3.firstName)
+
+
+
+    val emp4 =  EmpyloyeeMutipleConstruct2("Avinash4")
+    println(emp4.firstName)
+
+    val emp5 =  EmpyloyeeMutipleConstruct2("Avinash5", false)
+    println(emp5.firstName)
+    println(emp5.fullTime)
+
+    val emp6 =  EmpyloyeeSetAndGet("Avinash5")
+    println(emp6.firstName)
+    println(emp6.fullTime)
+
+
+
 }
 
  class Empyloyee constructor(firstName:String){
@@ -36,5 +52,35 @@ class EmpyloyeeShortway1 constructor(val firstName:String){
 
 class EmpyloyeeShortway2 (val firstName:String){
 
+
+}
+
+class EmpyloyeeMutipleConstruct2 (val firstName:String){
+
+    var  fullTime:Boolean = true;
+
+ constructor(firstName:String ,  fullTime:Boolean ):this(firstName) {
+
+
+
+ }
+
+}
+
+
+class EmpyloyeeSetAndGet (val firstName:String){
+
+    var  fullTime:Boolean = true
+
+    var firstNameGet = firstName
+
+    get(){
+        return field
+    }
+
+    set(value){
+         field = value
+
+    }
 
 }
--- Kotlin/1_Introduction/src/kotlinAccessModifier8/javacode/JavaEmployee.java
+++ Kotlin/1_Introduction/src/kotlinAccessModifier8/javacode/JavaEmployee.java
@@ -2,9 +2,17 @@ package kotlinAccessModifier8.javacode;
 
 public class JavaEmployee {
     private final String firstName;
+    private final boolean  fullTime;
 
     public JavaEmployee(String firstName){
         this.firstName = firstName;
+        this.fullTime = true;
+
+    }
+
+    public JavaEmployee(String firstName, boolean  fullTime){
+        this.firstName = firstName;
+        this.fullTime = fullTime;
 
     }
 }
