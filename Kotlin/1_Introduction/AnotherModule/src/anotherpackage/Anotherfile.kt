e372b8cdabe3aa42fb4bc56eaa3699691daec014
Avinash Tiwari <qwe123kids@gmail.com>
2018-11-22 12:48:59 -0700

    no message

---
+++ Kotlin/1_Introduction/AnotherModule/src/anotherpackage/Anotherfile.kt
@@ -0,0 +1,10 @@
+package anotherpackage
+
+import kotlinimport18.CompanyCommunications
+import kotlinimport18.toplevel
+
+fun main(args: Array<String>) {
+ toplevel("Avinash")
+
+   println( CompanyCommunications.getCopyrightLine())
+}
