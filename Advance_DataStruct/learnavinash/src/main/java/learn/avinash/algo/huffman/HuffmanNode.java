03a59ce58d6fd200fc3cc02ce7dae70fe3c327bc
avinash\avinash.t <qwe123kids@gmail.om>
2019-01-14 19:27:00 -0700

    Huffman

---
+++ Advance_DataStruct/learnavinash/src/main/java/learn/avinash/algo/huffman/HuffmanNode.java
@@ -0,0 +1,15 @@
+package learn.avinash.algo.huffman;
+
+public class HuffmanNode {
+
+    char c;
+    int frequency;
+
+    HuffmanNode left = null;
+    HuffmanNode right = null;
+
+    public HuffmanNode(char c, int frequency) {
+        this.c = c;
+        this.frequency = frequency;
+    }
+}
